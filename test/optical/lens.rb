48216942738f9aca7b2e9b630d81c5d4ccdb4558
Brandon Fosdick <bfoz@bfoz.net>
2014-07-06 14:54:39 -0700

    Added thickness attributes to Lens

--- test/optical/lens.rb
+++ test/optical/lens.rb
@@ -9,6 +9,18 @@ describe Optical::Lens do
 	lens.focal_length.must_equal 5
     end
 
+    it 'must have a center thickness' do
+	Optical::Lens.new(center_thickness: 5).center_thickness.must_equal 5
+    end
+
+    it 'must have an edge thickness' do
+	Optical::Lens.new(edge_thickness: 4).edge_thickness.must_equal 4
+    end
+
+    it 'must have a thickness attribute that is the maximum thickness' do
+	Optical::Lens.new(center_thickness:2, edge_thickness:5).thickness.must_equal 5
+    end
+
     it 'must have a numerical aperture' do
 	lens = Optical::Lens.new(radius:1, focal_length:5)
 	lens.numerical_aperture.must_be_close_to 0.196
